d803650514bb2d17daabd7f75484ee77437f2671
george <george.thomas@hart.com>
2016-03-29 15:56:34 -0700

    Adding an extra error for tracinking end of input

--- queue.go
+++ queue.go
@@ -1,54 +1,58 @@
 package metre
 
 import (
-    "fmt"
-    zmq "github.com/pebbe/zmq4"
+	"fmt"
+	zmq "github.com/pebbe/zmq4"
 )
 
 const queueFlag zmq.Flag = 0
 
 type Queue struct {
-    URI string
-    PushSocket *zmq.Socket // Socket to push messages to
-    PullSocket *zmq.Socket // SOcket to pull mesasges form
+	URI        string
+	PushSocket *zmq.Socket // Socket to push messages to
+	PullSocket *zmq.Socket // SOcket to pull mesasges form
 }
 
 // BindPush binds to the socket to push
 func (q Queue) BindPush() error {
-    return q.PushSocket.Bind(q.URI)
+	return q.PushSocket.Bind(q.URI)
 }
 
 // ConnectPull connects to the socket to listen for messages
 func (q Queue) ConnectPull() error {
-    return q.PullSocket.Connect(q.URI)
+	return q.PullSocket.Connect(q.URI)
 }
 
 // Push pushes and element onto the queue
 func (q Queue) Push(msg string) (int, error) {
-    result, err := q.PushSocket.Send(msg, 1)
-    return result, err
+	result, err := q.PushSocket.Send(msg, 1)
+	return result, err
 }
 
 // Pop pulls off the last element from the queue
-func (q Queue) Pop() string {
-    m, _ := q.PullSocket.Recv(queueFlag)
-    return m
+func (q Queue) Pop() (string, error) {
+	m, err := q.PullSocket.Recv(queueFlag)
+	if err != nil {
+		return "", err
+	} else {
+		return m, nil
+	}
 }
 
 // New acts as a queue constructor
 func NewQueue(uri string) (Queue, error) {
-    u := "tcp://" + uri
-    c, _ := zmq.NewContext()
+	u := "tcp://" + uri
+	c, _ := zmq.NewContext()
 
-    pullSoc, pullErr := c.NewSocket(zmq.PULL)
-    if pullErr != nil {
-        return Queue{}, fmt.Errorf("pull socket initialization failed: %v",  pullErr)
-    }
-    pushSoc, pushErr := c.NewSocket(zmq.PUSH)
-    if pushErr != nil {
-        return Queue{}, fmt.Errorf("push socket initialization failed: %v",  pushErr)
-    }
+	pullSoc, pullErr := c.NewSocket(zmq.PULL)
+	if pullErr != nil {
+		return Queue{}, fmt.Errorf("pull socket initialization failed: %v", pullErr)
+	}
+	pushSoc, pushErr := c.NewSocket(zmq.PUSH)
+	if pushErr != nil {
+		return Queue{}, fmt.Errorf("push socket initialization failed: %v", pushErr)
+	}
 
-    q := Queue{u, pushSoc, pullSoc}
-    return q, nil
+	q := Queue{u, pushSoc, pullSoc}
+	return q, nil
 }
